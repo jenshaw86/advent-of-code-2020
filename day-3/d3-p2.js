24f88af23dd0ef11da3f27ef3aee1e081a39c05b
Jenny Shaw <bloo.j.shaw@gmail.com>
2020-12-03 12:09:17 -0800

    Refactor Day 3 Part 2

--- day-3/d3-p2.js
+++ day-3/d3-p2.js
@@ -1,38 +1,36 @@
+const inputLength = 323;
+const slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]];
+let input = [];
+
 const readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-let input = [];
-
 rl.on('line', function(line){
     input.push(line);
 
-    if(input.length === 323) {
+    if(input.length === inputLength) {
         rl.close();
     }
 })
 
-const slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]];
-
 rl.on('close', function() {
-    let product = 1;
-    slopes.forEach(slope => product *= treeCollisions(input, slope));
-    console.log(product);
+    console.log(collisionProduct(slopes));
 });
 
+const collisionProduct = slopes => slopes.reduce((a,c) => a * treeCollisions(input, c), 1);
+
 const treeCollisions = (input, slope) => {
-    let treeCount = 0;
     const [r,d] = [...slope];
+    let treeCount = 0;
     let j = r; 
 
     for (let i = d; i < input.length; i += d) {
         j = j >= input[i].length ? j % input[i].length : j;
 
-        if (input[i][j] === "#") {
-            treeCount++;
-        }
+        if (input[i][j] === "#") treeCount++;
 
         j += r;
     }
